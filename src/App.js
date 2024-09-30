42040efefc46028a26d11d5379c27d3fcab2a416
Vb267 <vrushankbhosale@gmail.com>
2024-09-30 18:00:24 +0530

    Made changes in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./App.css";
 import PropertyList from "./components/PropertyList";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PropertyList />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          {/* Route for Property Listings (Home) */}
+          <Route path="/" exact>
+            <PropertyList />
+          </Route>
+
+          {/* Route for User Registration */}
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          {/* Route for User Login */}
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
